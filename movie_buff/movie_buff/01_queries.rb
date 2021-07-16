8c107a374dafc89ba0013608d8385e6bb174aef2
gamestar <rinayumiho@gmail.com>
2021-07-16 14:29:13 -0400

    01 done

--- movie_buff/movie_buff/01_queries.rb
+++ movie_buff/movie_buff/01_queries.rb
@@ -6,7 +6,7 @@ def it_was_ok
   # We can use ranges (a..b) inside a where method.
   #
   # Find the id, title, and score of all movies with scores between 2 and 3
-
+  Movie.select("id, title, score").where(score: 2..3)
 end
 
 def harrison_ford
@@ -20,7 +20,10 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
-
+  Actor.select("movies.id, movies.title")
+       .joins(:movies)
+       .where("actors.name = 'Harrison Ford' AND ord != 1")
+      
 end
 
 def biggest_cast
@@ -37,6 +40,11 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
+  Movie.select("movies.id, movies.title")
+       .joins(:castings)
+       .group(1)
+       .order("COUNT(*) DESC")
+       .limit(3)
 
 end
 
@@ -52,6 +60,9 @@ def directed_by_one_of(them)
   # Movie.where(yr: years)
   #
   # Find the id and title of all the movies directed by one of 'them'.
+  Actor.select('movies.id, movies.title')
+       .joins(:directed_movies)
+       .where(name: them)
 
 end
 
@@ -66,5 +77,6 @@ def movie_names_before_1940
   # improve performace for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
-
+  Movie.where("yr < 1940")
+       .pluck(:title)
 end
